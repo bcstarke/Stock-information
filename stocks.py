52d1bb7d6be5c598e081fe324e763e3e49f3e0bc
bcstarke <48607540+bcstarke@users.noreply.github.com>
2019-08-29 14:31:20 -0400

    Added comments

--- stocks.py
+++ stocks.py
@@ -2,15 +2,15 @@ import bs4
 import requests
 from bs4 import BeautifulSoup
 
+#function to prompt user to enter ticker
 def getStock():
     stock = input("Enter ticker: ")
-    return str(stock.upper())
-   
+    return str(stock.upper()) #converts input to upeprcase
+     
 def parsePrice():
-    #parsePrice.stock = input("Enter ticker: ")
-    r = requests.get('https://finance.yahoo.com/quote/' + str(getStock()) + '/')
+    r = requests.get('https://finance.yahoo.com/quote/' + str(getStock()) + '/') 
     soup = bs4.BeautifulSoup(r.text, "xml")
-    currentPrice = soup.find('div',{'class':'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text
+    currentPrice = soup.find('div',{'class':'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').text #parses yahoo finance XML for current price information
     return currentPrice
 
 while True:
